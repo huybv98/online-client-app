ce6f3f4006497721a844e9d6bc3878e5aa8651e5
Huybv98 <mcnb.1998@gmail.com>
2023-10-09 17:01:06 +0700

    Create component app

---
+++ src/components/AppInputTextArea/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+interface Props {
+}
+
+const AppInputTextArea  = ({ props }: Props)=> {
+
+    return (
+        <div>AppInputTextArea </div>
+    )
+}
+
+export default AppInputTextArea
---
+++ src/components/AppTabs/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+interface Props {
+}
+
+const AppTabs = ({ props }: Props)=> {
+
+    return (
+        <div>AppTabs</div>
+    )
+}
+
+export default AppTabs
